b321009fb48f85900ead6bdc20556e8b4266e2d3
Abdullah Alshalan <a.shalan443@gmail.com>
2026-01-07 14:48:03 +0300

    Add full screen raycast script

---
+++ macos/.raycast/toggle-full-screen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Toggle Full screen
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💻
+
+# Documentation:
+# @raycast.description Use raycast to make the active windows have maximum width and height 
+# @raycast.author Abdullah
+
+open -g raycast://extensions/raycast/window-management/maximize-width
+open -g raycast://extensions/raycast/window-management/maximize-height
+
